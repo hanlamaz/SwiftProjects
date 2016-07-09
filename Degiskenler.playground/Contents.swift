78effd8abfeb5b73226db25e3e96de3aa2c48672
hanlamaz <hanlamaz17@gmail.com>
2016-07-09 19:24:11 +0300

    Swiftdeki değişkenler

---
+++ Degiskenler.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var integer:Int = 15
+var float: Float = 1.5
+var double = 1.5
+
+var topla = 11+5
+var cikar = 11-5
+var carp = 11*5
+var bol = 11/5
+var mod = 11%5
+
+var dortislem = (topla + carp ) - (bol + cikar)
+var birlesmisstring = "Merhaba ," + "str değişkeni = " + str
+var ad = "Hüseyin"
+var soyad="Anlamaz"
+var advesoyad = ad + " " + soyad
+var advesoyad2 = "\(ad) \(soyad)" // Daha iyi olduğu söyleniyor
+
+var fiyat = " Fiyat " + "$\(topla)"
+let degismeyensayi = 5
+//degismeyensayi = 1 -> Hata
+
+
+
+
+
